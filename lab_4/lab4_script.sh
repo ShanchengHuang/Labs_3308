7015b62ee7e443374a6af1cf5e41d73844d4faca
ShanchengHuang <Shancheng.Huang@colorado.edu>
2020-09-19 15:04:53 -0600

    Lab_4

---
+++ lab_4/lab4_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Authors :ShanchengHuang
+# Date: 09/19/2020
+
+cp ~/var/log/syslog /home
+egreq "error" syslog | tee error_log_check.txt
+mail -s 'System Error' 2437065479@qq.com <<< 'testing message body' /home/error_log_check.txt
