2288c7f1927c7766c272a7a95a0a19036fda4709
JackHuang <70254746+ShanchengHuang@users.noreply.github.com>
2020-09-22 23:52:48 -0600

    Update lab4_script.sh

--- lab_4/lab4_script.sh
+++ lab_4/lab4_script.sh
@@ -4,4 +4,4 @@
 
 cp ~/var/log/syslog /home
 egreq "error" syslog | tee error_log_check.txt
-mail -s 'System Error' 2437065479@qq.com <<< 'testing message body' /home/error_log_check.txt
+mail -s 'System Error' 2437065479@qq.com <<< 'testing message body' /error_log_check.txt
