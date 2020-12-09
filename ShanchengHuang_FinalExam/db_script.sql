98b9f9e60ba2a988686d87e50187acf4f888b8b8
ShanchengHuang <Shancheng.Huang@colorado.edu>
2020-12-09 15:58:51 -0700

    test node.js

--- ShanchengHuang_FinalExam/db_script.sql
+++ ShanchengHuang_FinalExam/db_script.sql
@@ -1,7 +1,6 @@
 CREATE DATABASE book_review;
 
-CREATE TABLE
-IF NOT EXISTS table_review
+CREATE TABLE IF NOT EXISTS table_review
 (
 	id SMALLINT PRIMARY KEY,
 	book_title TEXT,
@@ -9,7 +8,4 @@ IF NOT EXISTS table_review
 	review_data DATE
 );
 
-INSERT INTO book_review
-    (id,book_title,book_title,review,review_data)
-VALUES
-    ('0', 'Books of Answer', 'GOD', 'very good, the books that you have to read about', '2020-12-9');
+INSERT INTO table_review (id,book_title,review,review_data) VALUES ('0', 'Books of Answer', 'very good, the books that you have to read about', '2020-12-9');
